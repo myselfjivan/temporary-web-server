968a32ddf6fe6fe210ce6abdeb0dc761ea6b43ad
myselfjivan <jivangahdage@gmail.com>
2016-04-04 20:16:38 +0530

    fileUpload gimp opensource

---
+++ third.php
@@ -0,0 +1,32 @@
+<?php
+// Script written by Adam Khoury for the following video exercise:
+// http://www.youtube.com/watch?v=7fTsf80RJ5w
+if (isset($_FILES['file_array'])) {
+    $name_array = $_FILES['file_array']['name'];
+    $tmp_name_array = $_FILES['file_array']['tmp_name'];
+    $type_array = $_FILES['file_array']['type'];
+    $size_array = $_FILES['file_array']['size'];
+    $error_array = $_FILES['file_array']['error'];
+    for ($i = 0; $i < count($tmp_name_array); $i++) {
+        if (move_uploaded_file($tmp_name_array[$i], "/var/www/html/temporaryWebServer/uploads/" . $name_array[$i])) {
+            echo $name_array[$i] . " upload is complete<br>";
+        } else {
+            echo "move_uploaded_file function failed for " . $name_array[$i] . "<br>";
+        }
+    }
+}
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+    </head>
+    <body>
+        <form action="" method="post" enctype="multipart/form-data">
+            <p><input type="file" name="file_array[]"></p>
+            <p><input type="file" name="file_array[]"></p>
+            <p><input type="file" name="file_array[]"></p>
+            <input type="submit" value="Upload all files">
+        </form>
+    </body>
+</html>
